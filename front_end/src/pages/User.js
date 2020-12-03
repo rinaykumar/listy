0976fae9369643c04f5622a45f844a1936713075
Girish Rawat <emailgirishrawat@gmail.com>
2020-12-03 09:08:47 +0530

    Added navbar, redesigned signup, login

--- front_end/src/pages/User.js
+++ front_end/src/pages/User.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ViewListings from "../components/ViewListings";
+import NavigationHeader  from "../components/NavigationHeader";
 
 const User = () => {
   return (
     <div>
+      <NavigationHeader/>
       <h2>User Route</h2>
       <div>
         <div>
