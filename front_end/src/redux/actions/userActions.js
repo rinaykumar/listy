acfaf4bae959c46b6916423cdf3eec304ed02179
Girish Rawat <emailgirishrawat@gmail.com>
2020-12-05 19:30:27 +0530

    added setIsLoggedIn userAction

--- front_end/src/redux/actions/userActions.js
+++ front_end/src/redux/actions/userActions.js
@@ -3,6 +3,11 @@ export const setUsername = (username) => ({
   username,
 });
 
+export const setIsLoggedIn = (isLoggedIn) => ({
+  type: 'USER_SET_LOGGED_IN',
+  isLoggedIn,
+});
+
 export const setPassword = (password) => ({
   type: "PASSWORD_SET",
   password,
