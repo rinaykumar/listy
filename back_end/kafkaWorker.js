1f1a4f5a8674415ad3cf066f191a3d54b550ee4a
bhavani-goruganthu <bgoruganthu@mail.sfsu.edu>
2020-12-19 00:27:57 -0800

    fixed kafka

--- back_end/kafkaWorker.js
+++ back_end/kafkaWorker.js
@@ -82,8 +82,8 @@ consumer.on("message", async (message) => {
     // finalImg100: finalImg100,
     // finalImg500: finalImg500,
   };
-  setTimeout(() => {
-    publisher.publish("processImage", JSON.stringify(userMsg));
-  }, 5000);
+  // setTimeout(() => {
+  publisher.publish("processImage", JSON.stringify(userMsg));
+  // }, 5000);
 });
 consumer.connect();
